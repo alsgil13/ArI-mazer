af3eb3fb6ef1b167e2f8875012d2a6676fee2fe0
alsgil13 <als.gil13@gmail.com>
2019-11-23 12:12:13 -0200

    Incluindo revelar caminho

--- labirinto.js
+++ labirinto.js
@@ -46,20 +46,17 @@ request.onload = function() {
             for (var x = 0; x < board[y].length; x++) {
             //Desenha o Muro
                 if (board[y][x] === 1) {
-                    
+                    ctx.fillStyle = "red";
                     ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
                 }
 
-                if(board[y][x] === -1){
+                if(board[y][x] ===  -1){
                     // var half = blockSize / 2;
                     // ctx.fillStyle = "white";
                     // ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
-                    // ctx.fill();
-                    ctx.beginPath();
-                    var half = blockSize / 2;
-                    ctx.fillStyle = "blue";
-                    ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
-                    ctx.fill();
+                    
+                    ctx.fillStyle = "red";
+                    ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
                 }
             
             }
@@ -70,11 +67,12 @@ request.onload = function() {
 
 
     //Desenha a bolinha do jogador
-        ctx.beginPath();
-        var half = blockSize / 2;
-        ctx.fillStyle = "blue";
-        ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
-        ctx.fill();
+
+            ctx.beginPath();
+            var half = blockSize / 2;
+            ctx.fillStyle = "blue";
+            ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
+            ctx.fill();
 
 
 
@@ -89,34 +87,34 @@ request.onload = function() {
 
 
     $(document).keyup(function(e) {
-    if ((e.which == 38) && canMove(player.x, player.y - 1)) { 
-        player.y--;
-        contador++;
-        console.log(player.x);
-        console.log(player.y);
-        
-    } else if ((e.which == 40) && canMove(player.x, player.y + 1)) { 
-        player.y++;
-        contador++;
-        console.log(player.x);
-        console.log(player.y);
-    } else if ((e.which == 37) && canMove(player.x - 1, player.y)) {
-        player.x--;
-        contador++;
-        console.log(player.x);
-        console.log(player.y);
-    } else if ((e.which == 39) && canMove(player.x + 1, player.y)) {
-        player.x++;
-        contador++;
-        console.log(player.x);
-        console.log(player.y);
-    }
-    if (player.x == saida.x && player.y == saida.y) {
-        estrelas = 1;
-        if(contador<tamanho){
-            estrelas = 3;
+        if ((e.which == 38) && canMove(player.x, player.y - 1)) { 
+            player.y--;
+            contador++;
+            console.log(player.x);
+            console.log(player.y);
+            
+        } else if ((e.which == 40) && canMove(player.x, player.y + 1)) { 
+            player.y++;
+            contador++;
+            console.log(player.x);
+            console.log(player.y);
+        } else if ((e.which == 37) && canMove(player.x - 1, player.y)) {
+            player.x--;
+            contador++;
+            console.log(player.x);
+            console.log(player.y);
+        } else if ((e.which == 39) && canMove(player.x + 1, player.y)) {
+            player.x++;
+            contador++;
+            console.log(player.x);
+            console.log(player.y);
         }
-        window.alert("Parabéns, você concluiu o labirinto com: " + contador + " passos\nVocê conseguiu "+ estrelas + " estrelas neste desafio");
+        if (player.x == saida.x && player.y == saida.y) {
+            estrelas = 1;
+            if(contador<tamanho){
+                estrelas = 3;
+            }
+            window.alert("Parabéns, você concluiu o labirinto com: " + contador + " passos\nVocê conseguiu "+ estrelas + " estrelas neste desafio");
     }
 
 
@@ -132,15 +130,106 @@ request.onload = function() {
     //     }
     // }    
     
+
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+          if ((new Date().getTime() - start) > milliseconds){
+            break;
+          }
+        }
+      }
+
+
     $('#revela').click(function(){
         console.log(caminho.length)
-        for(var x = 0; x < caminho.length; x++){
-            board[caminho[x]] = -1;
-            console.log(caminho[x]);
-            console.log(board[caminho[x]]);
+        
+        
+        var width = canvas.width();
+        var blockSize = width / board.length;
+        var ctx = canvas[0].getContext('2d');
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, width, width);
+        ctx.fillStyle = "white";
+        //Faz um loop através do tabuleiro para desenhar os muros
+        for (var y = 0; y < board.length; y++) {
+            for (var x = 0; x < board[y].length; x++) {
+            //Desenha o Muro
+                if (board[y][x] === 1) {
+                    ctx.fillStyle = "red";
+                    ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+                }
+
+                if(board[y][x] ===  -1){
+                    // var half = blockSize / 2;
+                    // ctx.fillStyle = "white";
+                    // ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
+                    
+                    // ctx.fillStyle = "red";
+                    // ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+                }
+            
+            }
         }
-        draw();        
+
+    
+        
+        
+        for(var x = 0; x < caminho.length; x++){
+            // board[caminho[x]] = -1;
+            // console.log(caminho[x]);
+            // console.log(board[caminho[x]]);
+            console.log(caminho[x][0]);
+
+
+            //--------------------------------------------
+
+            //--------------------------------------------
+
+            ctx.beginPath();
+            var half = blockSize / 2;
+            ctx.fillStyle = "grey";
+            ctx.arc(caminho[x][1] * blockSize + half, caminho[x][0] * blockSize + half, half, 0, 2 * Math.PI);
+            ctx.fill();
+            
+
+
+
+
+        }
+        ctx.beginPath();
+        var half = blockSize / 2;
+        ctx.fillStyle = "blue";
+        ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
+        ctx.fill();
+        // --------------------------------------------------------------
+
+        // var width = canvas.width();
+        // var blockSize = width / board.length;
+        // var ctx = canvas[0].getContext('2d');
+        // ctx.setTransform(1, 0, 0, 1, 0, 0);
+        // ctx.clearRect(0, 0, width, width);
+        // //ctx.fillStyle = "red";
+        // //Faz um loop através do tabuleiro para desenhar os muros
+        // for (var y = 0; y < board.length; y++) {
+        //     for (var x = 0; x < board[y].length; x++) {
+        //     //Desenha o Muro
+        //         if (board[y][x] === 1) {
+        //             ctx.fillStyle = "green";
+        //             ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+        //         }
+
+        //         if(board[y][x] === -1){
+        //             ctx.fillStyle = "red";
+        //             ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+        //         }
+            
+        //     }
+        // }
+
+
+
+        //--------------------------------------
     })
 
 }
-
